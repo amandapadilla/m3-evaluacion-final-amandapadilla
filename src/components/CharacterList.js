09989cd893c9694387db1af6c6da986c4d43210c
amandapadilla <amandapadillasantiago@gmail.com>
2019-09-24 19:47:19 +0200

    add conditionals for paint fails

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -5,25 +5,36 @@ import { Link } from "react-router-dom";
 
 const CharacterList = props => {
   const { characters } = props;
-  return (
-    <div className="list">
-      <ul className="list__list-type">
-        {characters.map(character => {
-          return (
-            <li className="list__item-character" key={character.id}>
-              <Link to={`/detail/ ${character.id}`}>
-                <CharacterCard
-                  name={character.name}
-                  image={character.image}
-                  species={character.species}
-                />
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+  if (characters.length === 0) {
+    return (
+      <div className="fail">
+        <h2 className="fail__message">
+          Hi! I'm Mr. Meeseeks, look at me! Sorry, there isn't a coincidence. Oh
+          no! I can't help you and will live foreveeeeeeeer!!!
+        </h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className="list">
+        <ul className="list__list-type">
+          {characters.map(character => {
+            return (
+              <li className="list__item-character" key={character.id}>
+                <Link to={`/detail/ ${character.id}`}>
+                  <CharacterCard
+                    name={character.name}
+                    image={character.image}
+                    species={character.species}
+                  />
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default CharacterList;
